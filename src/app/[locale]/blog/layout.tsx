683fb2ea2e64a3ddd859e7489d576a5ca03b34f7
申栋栋 <sailwebs@apecloud.com>
2025-02-28 22:46:42 +0800

    feat: add reports

--- src/app/[locale]/blog/layout.tsx
+++ src/app/[locale]/blog/layout.tsx
@@ -4,9 +4,13 @@ import { ArrowBack } from "@mui/icons-material";
 import { getI18n } from "@/locales/server";
 import { Box, Button } from "@mui/material";
 import { Link } from "@/components/Link";
+import { setStaticParamsLocale } from "next-international/server";
 
-export default async function BlogsLayout({ children }: { children: React.ReactNode }) {
-  const t = await getI18n()
+export default async function BlogsLayout({ children, params }: { children: React.ReactNode, params: Promise<{ locale: "zh" | "en" }> }) {
+  const { locale } = await params;
+  setStaticParamsLocale(locale);
+  const t = await getI18n();
+
   return (
     <>
       <Container sx={{ minHeight: "calc(100vh - 265px)", paddingBlock: 4 }} className="markdown-body">
