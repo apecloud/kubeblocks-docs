5fd327a809033400123bf505c28464b95d6806b1
申栋栋 <sailwebs@apecloud.com>
2025-02-28 23:20:33 +0800

    feat: add reports

--- src/app/[locale]/blog/[name]/layout.tsx
+++ src/app/[locale]/blog/[name]/layout.tsx
@@ -1,4 +1,8 @@
 import { setStaticParamsLocale } from "next-international/server";
+import { ArrowBack } from "@mui/icons-material";
+import { getI18n } from "@/locales/server";
+import { Box, Button } from "@mui/material";
+import { Link } from "@/components/Link";
 
 export default async function BlogsDetail({
   children,
@@ -9,6 +13,16 @@ export default async function BlogsDetail({
 }) {
   const { locale } = await params;
   setStaticParamsLocale(locale);
+  const t = await getI18n();
 
-  return children;
+  return (
+    <>
+      <Box sx={{ marginBottom: 4 }}>
+        <Button startIcon={<ArrowBack />} component={Link} href="/blog">
+          {t("actions.back")}
+        </Button>
+      </Box>
+      {children}
+    </>
+  );
 }
--- src/app/[locale]/blog/layout.tsx
+++ src/app/[locale]/blog/layout.tsx
@@ -1,28 +1,14 @@
 import Footer from "@/components/Footer";
 import { Container } from "@mui/material";
-import { ArrowBack } from "@mui/icons-material";
-import { getI18n } from "@/locales/server";
-import { Box, Button } from "@mui/material";
-import { Link } from "@/components/Link";
 import { setStaticParamsLocale } from "next-international/server";
 
 export default async function BlogsLayout({ children, params }: { children: React.ReactNode, params: Promise<{ locale: "zh" | "en" }> }) {
   const { locale } = await params;
   setStaticParamsLocale(locale);
-  const t = await getI18n();
 
   return (
     <>
       <Container sx={{ minHeight: "calc(100vh - 265px)", paddingBlock: 4 }} className="markdown-body">
-        <Box sx={{ marginBottom: 4 }}>
-          <Button
-            startIcon={<ArrowBack />}
-            component={Link}
-            href="/blog"
-          >
-            {t("actions.back")}
-          </Button>
-        </Box>
         {children}
       </Container>
       <Footer />
