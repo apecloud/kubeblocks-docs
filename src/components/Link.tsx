d1b742ef705204765743504e1b84a105d0a84e72
申栋栋 <sailwebs@apecloud.com>
2025-03-03 11:05:06 +0800

    feat: link anchor with animate

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { animateScroll } from 'react-scroll';
+
+
 import {
   Link as MuiLink,
   LinkProps as MuiLinkProps,
@@ -9,5 +12,16 @@ import NextLink, { LinkProps as NextLinkProps } from "next/link";
 export type LinkProps = MuiLinkProps & NextLinkProps;
 
 export function Link(props: LinkProps) {
-  return <MuiLink {...props} component={NextLink} />;
+  return <MuiLink {...props} component={NextLink} onClick={(e) => {
+    const anchorId = props.href?.match(/^#/) ? props.href.replace(/^#/, '') : undefined;
+    const anchor = anchorId && document.getElementById(anchorId);
+    const top = anchor && anchor.offsetTop
+    if(top) {
+      animateScroll.scrollTo(top - 75, {
+        duration: 300,
+        smooth: true,
+      })
+      e.preventDefault();
+    }
+  }} />;
 }
