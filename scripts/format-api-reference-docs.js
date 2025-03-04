89f4e5c1b96a8ed88e78cdd2c27b2ca815e69108
申栋栋 <sailwebs@apecloud.com>
2025-03-04 19:11:11 +0800

    fix: 0.9 zh docs

--- scripts/format-api-reference-docs.js
+++ scripts/format-api-reference-docs.js
@@ -4,21 +4,22 @@ const fs = require("fs");
 
 const ROOT_DIR = process.cwd();
 const files = [
-  `${ROOT_DIR}/docs/en/release-0-9/user_docs/developer/api-reference/add-on.mdx`,
-  `${ROOT_DIR}/docs/en/release-0-9/user_docs/developer/api-reference/backup.mdx`,
-  `${ROOT_DIR}/docs/en/release-0-9/user_docs/developer/api-reference/cluster.mdx`,
+  `${ROOT_DIR}/docs/zh/release-0_9/user_docs/developer/api-reference/add-on.mdx`,
+  `${ROOT_DIR}/docs/zh/release-0_9/user_docs/developer/api-reference/backup.mdx`,
+  `${ROOT_DIR}/docs/zh/release-0_9/user_docs/developer/api-reference/cluster.mdx`,
 ]
 
 files
 .filter(file => fs.existsSync(file)) // check exists
 .forEach(file => {
+  console.log(file)
   const content = String(fs.readFileSync(file))
     .replace(/<h3([^>]*)>/g, (match, attr) => '<h3' + attr + '>\n')
     .replace(/<\/h3>/g, '\n</h3>')
     .replace(/<pre><code([^>]*)>/g, (match, attr) => '<pre>\n<code' + attr + '>\n')
     .replace(/<\/code><\/pre>/g, '</code>\n</pre>')
     .replace(/<code>(.*?)\s*<\/code>/g, (match, content) => '`' + content + '`')
-    .replace(/<p>/g, '<p>\n')
+    .replace(/<p>/g, '\n<p>\n')
     .replace(/<\/p>/g, '\n</p>')
     .replace(/<li>/g, '<li>\n')
     .replace(/<\/li>/g, '\n</li>')
