729af5625639421077bf050d04329edc00245bd8
申栋栋 <sailwebs@apecloud.com>
2025-11-26 09:41:30 +0800

    fix: email validate

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,14 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const ConstactUsFields = {
   username: z.string().min(1),
-  email: z.string().email(),
+  email: z
+    .string()
+    .email()
+    .refine((email) => {
+      const domain = email.split("@")[1];
+      return !["qq.com", "gmail.com", "163.com", "126.com"].includes(domain);
+    }),
   company: z.string().optional(),
   messages: z.string().max(300).optional(),
   url: z.string().optional(),
